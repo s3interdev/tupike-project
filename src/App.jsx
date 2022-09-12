c5b9df429cda0ac235b485ffbe9f8e726f26ded3
Michael Owuor <michael.owuor@gmail.com>
2022-09-12 13:48:41 +0300

    feat: router and pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/home';
+import Create from './pages/create/create';
+import Search from './pages/search/search';
+import Recipe from './pages/recipe/recipe';
 
 const App = () => {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route index element={<Home />} />
+					<Route path="/create" element={<Create />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/recipe/:id" element={<Recipe />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
