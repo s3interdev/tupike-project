910da2eadade67a450eb5117a2f457e634a797a8
Michael Owuor <michael.owuor@gmail.com>
2022-09-13 17:25:36 +0300

    feat: search results page

--- src/components/recipe-list.jsx
+++ src/components/recipe-list.jsx
@@ -7,7 +7,7 @@ const RecipeList = ({ recipes }) => {
 				<div key={recipe.id} className="card">
 					<h3>{recipe.title}</h3>
 					<p>{recipe.cookingTime} to make</p>
-					<div>{recipe.method.substring(0, 100)}...</div>
+					<div className="line-clamp-2">{recipe.method}</div>
 					<Link to={`/recipes/${recipe.id}`}>Cook This</Link>
 				</div>
 			))}
