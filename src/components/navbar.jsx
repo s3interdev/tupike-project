2b9d73315cceb7b86821b66d16e8882e9583c6a0
Michael Owuor <michael.owuor@gmail.com>
2022-09-13 19:43:41 +0300

    chore: accessing context values

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from '../context/theme-context';
 import SearchBar from './search-bar';
 
 const Navbar = () => {
+	const { color } = useContext(ThemeContext);
+
 	return (
-		<div className="navbar">
+		<div className="navbar" style={{ background: color }}>
 			<nav>
 				<Link to="/" className="brand">
 					<h1>Tupike</h1>
