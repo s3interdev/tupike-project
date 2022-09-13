6a5627e228570bcb7dbf8cf2125cfb800c92b6c6
Michael Owuor <michael.owuor@gmail.com>
2022-09-13 13:19:14 +0300

    feat: adding multiple ingredients

--- src/pages/create/create.jsx
+++ src/pages/create/create.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const Create = () => {
 	const [title, setTitle] = useState('');
 	const [method, setMethod] = useState('');
 	const [cookingTime, setCookingTime] = useState('');
+	const [newIngredient, setNewIngredient] = useState('');
+	const [ingredients, setIngredients] = useState([]);
+	const ingredientInput = useRef(null);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		console.log(title, method, cookingTime, ingredients);
+	};
 
-		console.log(title, method, cookingTime);
+	const handleAddIngredinet = (e) => {
+		e.preventDefault();
+		const ingredient = newIngredient.trim();
+
+		if (ingredient && !ingredients.includes(ingredient)) {
+			setIngredients((previousIngredients) => [...previousIngredients, newIngredient]);
+		}
+
+		setNewIngredient('');
+		ingredientInput.current.focus();
 	};
 
 	return (
@@ -25,7 +39,26 @@ const Create = () => {
 					/>
 				</label>
 
-				{/** ingredients will go here */}
+				<label>
+					<span>Recipe Ingredients:</span>
+					<div className="ingredients">
+						<input
+							type="text"
+							onChange={(e) => setNewIngredient(e.target.value)}
+							value={newIngredient}
+							ref={ingredientInput}
+						/>
+						<button onClick={handleAddIngredinet} className="btn">
+							Add
+						</button>
+					</div>
+				</label>
+				<p>
+					Current Ingredients:{' '}
+					{ingredients.map((i) => (
+						<em key={i}>{i}, </em>
+					))}
+				</p>
 
 				<label>
 					<span>Recipe Method:</span>
