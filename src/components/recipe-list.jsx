1f98707045c0576b269e41dddc3fcfa862dbeedb
Michael Owuor <michael.owuor@gmail.com>
2022-09-13 17:32:48 +0300

    chore: finishing touches

--- src/components/recipe-list.jsx
+++ src/components/recipe-list.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
 
 const RecipeList = ({ recipes }) => {
+	if (recipes.length === 0) {
+		return (
+			<div className="error">
+				Sorry, there are no recipes with that search term to load...
+			</div>
+		);
+	}
+
 	return (
 		<div className="recipe-list">
 			{recipes.map((recipe) => (
